238b5d40b3a4068961a5c8f474aea788d71c665f
stwaiss <stwaiss@uwm.edu>
2016-11-05 15:18:10 -0500

    Initial Log-in Code
    Still need to redirect based on credentials

--- ask_Monguss/main.py
+++ ask_Monguss/main.py
@@ -15,6 +15,8 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
 from student import Student
 from instructor import Instructor
 from course import Course
@@ -23,10 +25,87 @@ from question import Question
 from timestamp import Timestamp
 from reply import Reply
 
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+
 class MainHandler(webapp2.RequestHandler):
+    postedUsername = ""
+    postedPassword = ""
+
     def get(self):
-        self.response.write('Hello world!')
+        template = JINJA_ENVIRONMENT.get_template('HTML/ePantherID_Log-in.html')
+        self.response.write(template.render())
+
+    def post(self):
+        self.postedUsername = self.request.get('ePantherID')
+        self.postedPassword = self.request.get('password')
+
+class LoginHandler(webapp2.RequestHandler):
+    def checkForMatch(self, postedUsername, postedPassword):
+        #open text file
+        users = open('usernames.txt', 'r')
+
+        #iterate over text file looking for match
+        for line in users:
+            infoList = line.strip().split(',')
+
+            if infoList[0] == MainHandler.postedUsername:
+                if infoList[1] == MainHandler.postedPassword:
+                    #return isInstructor value
+                    if infoList[2] == 0:
+                        return 0
+                    else:
+                        return 1
+        #if user is not matched, return -1
+        return -1
+
+    def post(self):
+        match = self.checkForMatch(MainHandler.postedUsername, MainHandler.postedPassword)
+        values = {
+            'credentials': match,
+            'username': MainHandler.postedUsername,
+            'password': MainHandler.postedPassword
+        }
+        template = JINJA_ENVIRONMENT.get_template('HTML/login_dummy.html')
+        self.response.write(template.render(values))
+
+
+class StudentLandingPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('HTML/Student_home.html')
+        self.response.write(template.render())
+
+class InstructorLandingPage(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('HTML/Instructor_home.html')
+        self.response.write(template.render())
+
+class AccountCreationHandler(webapp2.RequestHandler):
+    def get(self):
+        #Render HTML
+        pass
+
+    def post(self):
+        pass
+        # username = self.request.get("epantherID")
+        # password = self.request.get("password")
+        #
+        # type = self.request.get("type")
+        #
+        #
+        #open file
+        #check file for matching user name
+        #if matched, give error, if not, write to file
+
+
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/login', LoginHandler),
+    ('/student', StudentLandingPage),
+    ('/instructor', InstructorLandingPage),
+    ('/create', AccountCreationHandler)
+
 ], debug=True)
