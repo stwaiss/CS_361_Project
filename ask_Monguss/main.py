a5ce8e2f83b93edb20cb336d86854b46857b92d5
stwaiss <stwaiss@uwm.edu>
2016-11-07 11:08:27 -0600

    AccountCreation push
    AccountCreation handler "works". GAE does not support writing to local
    filesystem, so inputs cannot be saved to file. This will be fixed when
    migrating to NDB

--- ask_Monguss/main.py
+++ ask_Monguss/main.py
@@ -79,7 +79,6 @@ class LoginHandler(webapp2.RequestHandler):
 
         self.match = self.checkForMatch(self.postedUsername, self.postedPassword)
 
-
         values = {
             'credentials': self.match,
             'username': self.postedUsername,
@@ -89,26 +88,23 @@ class LoginHandler(webapp2.RequestHandler):
         self.response.write(template.render(values))
 
 
-class StudentLandingPage(webapp2.RequestHandler):
-    def get(self):
+class StudentLandingPageHandler(webapp2.RequestHandler):
+    def post(self):
         template = JINJA_ENVIRONMENT.get_template('HTML/Student_home.html')
         self.response.write(template.render())
 
-    def post(self):
-        pass
 
-class InstructorLandingPage(webapp2.RequestHandler):
-    def get(self):
+class InstructorLandingPageHandler(webapp2.RequestHandler):
+    def post(self):
         template = JINJA_ENVIRONMENT.get_template('HTML/Instructor_home.html')
         self.response.write(template.render())
 
-    def post(self):
-        pass
 
 class AccountCreationHandler(webapp2.RequestHandler):
     def get(self):
         #Render HTML
-        pass
+        template = JINJA_ENVIRONMENT.get_template('HTML/AccountCreation.html')
+        self.response.write(template.render())
 
     def post(self):
         pass
@@ -117,20 +113,45 @@ class AccountCreationHandler(webapp2.RequestHandler):
         #
         # type = self.request.get("type")
         #
-        #
         #open file
         #check file for matching user name
         #if matched, give error, if not, write to file
 
+        username=self.request.get('ePantherID')
+        password=self.request.get('password')
+        credential=self.request.get('credential')
 
+        users=open('usernames.txt', 'r')
 
+        userAlreadyExists = 0
+        for line in users:
+            infoList = line.strip().split(',')
+
+            if infoList[0] == str(username):
+                userAlreadyExists = 1
+                values = {
+                    'userAlreadyExists': userAlreadyExists,
+                    'username': username
+                }
+                #Refresh and write error message
+                template = JINJA_ENVIRONMENT.get_template('HTML/AccountCreation.html')
+                self.response.write(template.render(values))
+                break
+
+        #### GAE does not support writing to local text files.
+        # if credential == "instructor":
+        #    users.write(username + ',' + password + ',1')
+        # elif credential == "student":
+        #    users.write(username + ',' + password + ',0')
+
+        users.close()
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/login', LoginHandler),
-    ('/student', StudentLandingPage),
-    ('/instructor', InstructorLandingPage),
+    ('/student', StudentLandingPageHandler),
+    ('/instructor', InstructorLandingPageHandler),
     ('/create', AccountCreationHandler)
 
 ], debug=True)
